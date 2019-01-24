faa92fed9206dd4cbb1afc2d626bd753ec1ddd2b
hbx <huanghbx97@outlook.com>
2019-01-24 09:57:38 -0500

    removed unnecessary files

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hbx/git/ekf_slam
-# Build directory: /home/hbx/git/ekf_slam/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(src)
-subdirs(test)
